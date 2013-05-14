2675a89c998c09b47f2624fb62061ffaa3a582cb
Jaroslaw Juda <jarojuda@gmail.com>
2013-05-14 23:49:36 +0200

    clean

--- examples/microchip-mplabx/pic18.X/build/pic18f97j60/production/obj_pic18f97j60-family/multihop.p1.d
+++
@@ -1,6 +0,0 @@
- build/pic18f97j60/production/obj_pic18f97j60-family/multihop.d  \
- build/pic18f97j60/production/obj_pic18f97j60-family/multihop.p1:  \
- build/pic18f97j60/production/obj_pic18f97j60-family/multihop.p1.c  \
-../../../../contiki/core/dev/spi.h  \
-../../../../contiki/cpu/microchip-mplabx/MAPLv2013-02-15/Include/Compiler.h  \
-../../../../contiki/cpu/microchip-mplabx/MAPLv2013-02-15/Include/GenericTypeDefs.h 
